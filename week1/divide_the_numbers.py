3ab032634839096fc90e39da7cf1fb33bc691423
Pasalavijay799 <158278987+Pasalavijay799@users.noreply.github.com>
2024-02-08 08:58:15 +0530

    Update divide_the_numbers.py

--- week1/divide_the_numbers.py
+++ week1/divide_the_numbers.py
@@ -1,1 +1,21 @@
+# divide the numbers
+def check(a):
+	if(a != "" and a[0] == '-'):
+		return 1
+	else:
+		return 0	
+a=input("enter the number")
+b=input("enter the number")
+acopy = a.replace(".", "")
+bcopy = b.replace(".", "")
+if (check(a)):
+	acopy = acopy.replace("-", "")
+if (check(b)):
+	bcopy = bcopy.replace("-", "") 
+
+if ((a.isnumeric() or acopy.isnumeric()) and (b.isnumeric() or bcopy.isnumeric()) ):
+	print(float(a) / float(b))
+else:
+	print(" data is not enterd correctly")
+
 
