fa42a9b3068916b89475169e5e11370830bc8f7d
Pasalavijay799 <158278987+Pasalavijay799@users.noreply.github.com>
2024-02-21 14:07:13 +0530

    day1.py

--- week1/day1.py
+++ week1/day1.py
@@ -5,23 +5,44 @@ print(a1+b1)
 a1=input("enter a number")
 b1=input("enter a number")
 print(a1+b1)
-#dot produt................................................
-a=[2,3,4]
-b=[5,0,7]
+#dot produt........................................................
+n=int(input("enter the how many number want to add : "))
+a=[]
+b=[]
+for i in range(n):
+	tem=int(input("enter the number vector1"))
+	temp2=int(input("enter the number vector2"))
+	a.append(tem)
+	b.append(temp2)
 new=0
+print(a)
+print(b)
 for i in range(3):
 	new+=(a[i]*b[i])
-print(new)
-# divide the numbers............................................
+print("the dot product above vectors: ",new)
+# divide the numbers.................................................................
+def check(a):
+	if(a != "" and a[0] == '-'):
+		return 1
+	else:
+		return 0	
+
+
 a=input("enter the number")
 b=input("enter the number")
 acopy = a.replace(".", "")
 bcopy = b.replace(".", "")
+if (check(a)):
+	acopy = acopy.replace("-", "")
+if (check(b)):
+	bcopy = bcopy.replace("-", "") 
 
-if ((a.isnumeric() or acopy.isnumeric() or int(a)<0) and (b.isnumeric() or bcopy.isnumeric() or int(b)<0) ):
+if ((a.isnumeric() or acopy.isnumeric()) and (b.isnumeric() or bcopy.isnumeric()) ):
 	print(float(a) / float(b))
 else:
-	print("Given data is wrong")
+	print(" data is not enterd correctly")
+
+
 #determinent of matrix...............................................................................
 matrix =[[0,0,0],[0,0,0],[0,0,0]]
 
